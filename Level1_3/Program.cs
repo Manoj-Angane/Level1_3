e6c45e1d1a4e4be776f38fbd5d988b09b7d9dfe3
manojangane@gmail.com <manojangane@gmail.com>
2019-01-22 15:47:40 -0500

    Version1

--- Level1_3/Program.cs
+++ Level1_3/Program.cs
@@ -6,7 +6,30 @@ namespace Level1_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a Number : ");
+            string input = Console.ReadLine();
+            try
+            {
+                int a = int.Parse(input);
+                if(a<=0)
+                {
+                    Console.WriteLine("Number entered is niether Odd nor Even : " + input);
+                }
+                else if (a / 2 == 1)
+                {
+                    Console.WriteLine("Number entered is Odd : " + input);
+                }
+                else
+                {
+                    Console.WriteLine("Number entered is Even : " + input);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Thank You!!..Hit any key to exit");
+            Console.ReadKey(true);
         }
     }
 }
